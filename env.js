edd3edcd03c88fe1c5d20f2fec015d29fdf190b7
TristanCavelier <t.cavelier@free.fr>
2015-07-21 17:11:42 +0200

    Create env.js

---
+++ env.js
@@ -0,0 +1,13 @@
+/*jslint indent: 2 */
+(function () {
+  "use strict";
+
+  /*! Copyright (c) 2015 Tristan Cavelier <t.cavelier@free.fr>
+      This program is free software. It comes without any warranty, to
+      the extent permitted by applicable law. You can redistribute it
+      and/or modify it under the terms of the Do What The Fuck You Want
+      To Public License, Version 2, as published by Sam Hocevar. See
+      http://www.wtfpl.net/ for more details. */
+
+  return;
+}());
