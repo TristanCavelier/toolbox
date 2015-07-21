9f8b1652a28d1979c6c1c291d57e5bf9b3ea5d3d
TristanCavelier <t.cavelier@free.fr>
2015-07-21 21:41:38 +0000

    env.js: add newXMLHttpRequestPromise function

--- env.js
+++ env.js
@@ -275,6 +275,87 @@
   }
   env.textToHTTPHeadersObject = textToHTTPHeadersObject;
 
+  function newXMLHttpRequestPromise(param) {
+    /**
+     *    newXMLHttpRequestPromise({url: location, responseType: "text"}).then(propertyGetter("data"));
+     *    newXMLHttpRequestPromise({url: location}).then(propertyGetter("Content-Length"));
+     *
+     * Send request with XHR and return a promise. xhr.onload: The promise is
+     * resolved when the status code is lower than 400 with a forged response
+     * object as resolved value. xhr.onerror: reject with an Error (with status
+     * code in status property) as rejected value.
+     *
+     * @param  {Object} param The parameters
+     * @param  {String} param.url The url (can be `param.uri`)
+     * @param  {String} [param.method="GET"] The request method
+     * @param  {String} [param.responseType=""] The data type to retrieve
+     * @param  {String} [param.overrideMimeType] The mime type to override
+     * @param  {Object} [param.headers] The headers to send
+     * @param  {Any} [param.data] The data to send
+     * @param  {Boolean} [param.withCredentials] Tell the browser to use
+     *   credentials
+     * @param  {Object} [param.xhrFields] The other xhr fields to fill
+     * @param  {Boolean} [param.getEvent] Tell the method to return the
+     *   response event.
+     * @param  {Function} [param.beforeSend] A function called just before the
+     *   send request. The first parameter of this function is the XHR object.
+     * @return {CancellablePromise<Object>} Response object is like { data: .., header1: ..,
+     *   header2: .., ... }
+     */
+
+    /*global XMLHttpRequest */
+    var d = env.newCancellableDeferred(), xhr = new XMLHttpRequest(), k;
+    d.oncancel = function () { xhr.abort(); };
+    xhr.open((param.method || "GET").toUpperCase(), param.url, true);
+    xhr.responseType = param.responseType || "";
+    if (param.overrideMimeType) {
+      xhr.overrideMimeType(param.overrideMimeType);
+    }
+    if (param.withCredentials !== undefined) {
+      xhr.withCredentials = param.withCredentials;
+    }
+    if (param.headers) {
+      for (k in param.headers) {
+        if (param.headers.hasOwnProperty(k)) {
+          xhr.setRequestHeader(k, param.headers[k]);
+        }
+      }
+    }
+    xhr.addEventListener("load", function (e) {
+      if (param.getEvent) { return d.resolve(e); }
+      var r;
+      if (e.target.status < 400) {
+        r = env.textToHTTPHeadersObject(e.target.getAllResponseHeaders());
+        r.data = e.target.response;
+        return d.resolve(r);
+      }
+      r = new Error("request: " + (e.target.statusText || "unknown error"));
+      r.status = e.target.status;
+      return d.reject(r);
+    }, false);
+    xhr.addEventListener("error", function (e) {
+      if (param.getEvent) { return d.resolve(e); }
+      return d.reject(new Error("request error"));
+    }, false);
+    xhr.addEventListener("abort", function (e) {
+      if (param.getEvent) { return d.resolve(e); }
+      return d.reject(new Error("request aborted"));
+    }, false);
+    if (param.xhrFields) {
+      for (k in param.xhrFields) {
+        if (param.xhrFields.hasOwnProperty(k)) {
+          xhr[k] = param.xhrFields[k];
+        }
+      }
+    }
+    if (typeof param.beforeSend === 'function') {
+      param.beforeSend(xhr);
+    }
+    xhr.send(param.data);
+    return d.promise;
+  }
+  env.newXMLHttpRequestPromise = newXMLHttpRequestPromise;
+
   //////////////////////////////////////////////////////////////////////
 
   return env;
