422f82e29b4f623e4ec3af00ebc25198d8683871
TristanCavelier <t.cavelier@free.fr>
2015-07-24 14:41:04 +0200

    env.js: add generateUuid function

--- env.js
+++ env.js
@@ -267,6 +267,34 @@
   }
   env.spawnPromise = spawnPromise;
 
+  ////////////
+  // Random //
+  ////////////
+
+  function generateUuid() {
+    /**
+     * An Universal Unique ID generator
+     *
+     * @return {String} The new UUID.
+     */
+    function S4() {
+      return ("0000" + Math.floor(
+        Math.random() * 0x10000
+      ).toString(16)).slice(-4);
+    }
+    function S8() {
+      return ("00000000" + Math.floor(
+        Math.random() * 0x100000000
+      ).toString(16)).slice(-8);
+    }
+    return S8() + "-" +
+      S4() + "-" +
+      S4() + "-" +
+      S4() + "-" +
+      S8() + S4();
+  }
+  env.generateUuid = generateUuid;
+
   //////////////////////
   // DOM Manipulation //
   //////////////////////
