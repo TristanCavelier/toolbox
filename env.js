95acaa9199bf0c84890818b4efa541dfff1bb759
TristanCavelier <t.cavelier@free.fr>
2015-08-16 20:23:36 +0000

    lint

--- env.js
+++ env.js
@@ -277,21 +277,21 @@
      *
      * @return {String} The new UUID.
      */
-    function S4() {
+    function s4() {
       return ("0000" + Math.floor(
         Math.random() * 0x10000
       ).toString(16)).slice(-4);
     }
-    function S8() {
+    function s8() {
       return ("00000000" + Math.floor(
         Math.random() * 0x100000000
       ).toString(16)).slice(-8);
     }
-    return S8() + "-" +
-      S4() + "-" +
-      S4() + "-" +
-      S4() + "-" +
-      S8() + S4();
+    return s8() + "-" +
+      s4() + "-" +
+      s4() + "-" +
+      s4() + "-" +
+      s8() + s4();
   }
   env.generateUuid = generateUuid;
 
