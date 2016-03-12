f1ba71f8181fdacf2d6f0b280131001fe23823ae
TristanCavelier <t.cavelier@free.fr>
2016-03-12 23:53:48 +0000

    Update env.eventmanagers.js

--- env.eventmanagers.js
+++ env.eventmanagers.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2 */
-(function envEventManagers(env) {
+(function (env) {
   "use strict";
 
   /*! Copyright (c) 2015-2016 Tristan Cavelier <t.cavelier@free.fr>
@@ -9,62 +9,100 @@
       To Public License, Version 2, as published by Sam Hocevar. See
       http://www.wtfpl.net/ for more details. */
 
-  // provides: env.{,new}EventManager
-  // - Force to have private listeners like in html5 EventTarget objects.
-  // - `dispatchEvent` is synchronous function, but it can be called like this: `setTimeout(em.dispatchEvent.bind(em), 0, event);`
-
-  var wm = typeof WeakMap === "function" ? new WeakMap() : {get: function (a) { return a; }, set: function () { return; }};
-
-  function priv(o) {
-    var tmp = wm.get(o);
-    if (tmp) { return tmp; }
-    wm.set(o, {});
-    return wm.get(o);
+  /*jslint vars: true */
+  var log = console.log.bind(console);
+  var warn = console.warn.bind(console);
+  function test(name, taskFn, timeout) {
+    return env.Task.raceWinOrCancel([
+      env.task(taskFn),
+      env.Task.sequence([env.sleep.bind(null, timeout || 1000), function () { throw new Error("test timeout (" + name + ")"); }])
+    ]).catch(function (v) {
+      warn(name);
+      warn(v);
+    });
   }
 
-  function EventManager() {
-    // can be mixed in with:
-    //     env.mixObjectProperties(Constructor.prototype, EventManager.prototype);
-
-    // API stability level: 1 - Experimental
-    return;
-  }
-  EventManager.prototype.addEventListener = function (type, listener) {
-    if (typeof listener !== "function") { return; }
-    var em = priv(this), key = "[[EventManagerListeners:" + type + "]]";
-    if (em[key]) {
-      em[key].push(listener);
-    } else {
-      em[key] = [listener];
-    }
-  };
-  EventManager.prototype.removeEventListener = function (type, listener) {
-    /*jslint plusplus: true */
-    var em = priv(this), key = "[[EventManagerListeners:" + type + "]]", listeners = em[key] || [], i, l = listeners.length;
-    for (i = 0; i < l; i += 1) {
-      if (listeners[i] === listener) {
-        if (l === 1) {
-          delete em[key];
-          return;
-        }
-        while (i < l) { listeners[i] = listeners[++i]; }
-        listeners.length -= 1;
-        return;
-      }
-    }
-  };
-  EventManager.prototype.dispatchEvent = function (event) {
-    var em = priv(this), key = "[[EventManagerListeners:" + event.type + "]]", key2 = "on" + event.type, listeners = em[key] || [], i, l = listeners.length;
-    if (typeof this[key2] === "function") {
-      try { this[key2](event); } catch (ignore) {}
-    }
-    for (i = 0; i < l; i += 1) {
-      try { listeners[i](event); } catch (ignore) {}
-    }
-  };
-
-  env.registerLib(envEventManagers);
-  env.EventManager = EventManager;
-  env.newEventManager = function () { var c = env.EventManager, o = Object.create(c.prototype); c.apply(o, arguments); return o; };
+  //////////////////////////////////////////////
+  // Channel tests
+  test("'next' gets sent value", function* () {
+    var chan = env.newChannel(), t1 = chan.send("test1 coucou"), v;
+    v = yield chan.next();
+    if (v.value !== "test1 coucou") { throw "wrong value"; }
+    yield t1;
+  });
+  test("same with buffered channel", function* () {
+    var chan = env.newChannel(1), v;
+    yield chan.send("test3 coucou");
+    v = yield chan.next();
+    if (v.value !== "test3 coucou") { throw "wrong value"; }
+  });
+  test("'next' can be cancelled without blocking another 'next' call", function* () {
+    var chan = env.newChannel(), nexting, nexting2, v;
+    nexting = chan.next();
+    nexting2 = chan.next();
+    nexting.catch(function () { return; });
+    nexting.cancel();
+    yield;
+    yield chan.send("yeah");
+    v = yield nexting2;
+    if (v.value !== "yeah") { throw "cancelled next still blocks anther next"; }
+  });
+  test("'send' sends to waiting next", function* () {
+    var chan = env.newChannel(), t1, v;
+    t1 = chan.next();
+    yield chan.send("test2 coucou");
+    v = yield t1;
+    if (v.value !== "test2 coucou") { throw "wrong value"; }
+  });
+  test("'close' a channel during waiting next", function* () {
+    var chan = env.newChannel(), v;
+    env.sleep(100).then(chan.close.bind(chan));
+    v = yield chan.next();
+    if (v.done !== true) { throw "wrong value"; }
+  });
+  test("'Channel.select' gets first responding next", function* () {
+    var chan = env.newChannel(), chan2 = env.newChannel(), v;
+    env.sleep(100).then(chan.send.bind(chan, "test5 coucou"));
+    env.sleep(500).then(chan2.send.bind(chan2, "test5 hello"));
+    v = yield env.Channel.select([
+      [chan2, function () { warn("should not happen"); }],
+      [chan, function (v) { return v.value; }]
+    ]);
+    if (v !== "test5 coucou") { throw "wrong value"; }
+  });
+  test("'Channel.select' gets run default if no next are available now", function* () {
+    var chan = env.newChannel(), chan2 = env.newChannel(), v;
+    env.sleep(100).then(chan.send.bind(chan, "test6 coucou"));
+    env.sleep(500).then(chan2.send.bind(chan2, "test6 hello"));
+    v = yield env.Channel.select([
+      [chan2, function () { warn("should not happen"); }],
+      [chan, function (v) { return v; }],
+      function () { return "default"; }
+    ]);
+    if (v !== "default") { throw "wrong value"; }
+  });
+  test("'Channel.select' gets run available not default", function* () {
+    var chan = env.newChannel(), chan2 = env.newChannel(), v;
+    chan.send("test coucou")
+    env.sleep(500).then(chan2.send.bind(chan2, "test hello"));
+    v = yield env.Channel.select([
+      [chan2, function () { warn("should not happen"); }],
+      [chan, function (v) { return v.value; }],
+      function () { return "default"; }
+    ]);
+    if (v !== "test coucou") { throw "wrong value"; }
+  });
+  test("'Channel.select' cancels next before calling callback", function* () {
+    var chan = env.newChannel(), chan2 = env.newChannel(), v;
+    env.sleep(100).then(chan.send.bind(chan, "test7 coucou")).then(null, warn.bind("should not happen"));
+    env.sleep(500).then(chan2.send.bind(chan2, "test7 hello")).then(warn.bind(null, "should not happen"), warn.bind("should not happen"));
+    v = yield env.Channel.select([
+      [chan2, function () { warn("should not happen"); }],
+      [chan, function (v) {
+        return env.sleep(600).then(function () { return v.value; });
+      }]
+    ]);
+    if (v !== "test7 coucou") { throw "wrong value"; }
+  });
 
 }(env));
